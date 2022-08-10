be33fc6c6c30ac77f073d6229b998b52fc0ddd37
Oby <obyli214@gmail.com>
2022-08-10 15:39:07 -0700

    added like button that logs the liked user

--- src/components/PersonCard/PersonCard.jsx
+++ src/components/PersonCard/PersonCard.jsx
@@ -5,8 +5,15 @@ export default function PersonCard({ person}) {
     let hobbies = person.hobbies.replaceAll(",", "")
     let hobbiesArr = hobbies.split(" ")
 
+    function likePerson() {
+        console.log(person.displayName)
+    }
+
     return (
         <div className="PersonCard" style={{ backgroundImage: `url(${person.image})`}}>
+            <div className="Heart-wrapper">
+            <button onClick={likePerson} className="Heart-btn"></button>
+            </div>
             <div className="PersonCard-wrapper">
             <h3>{person.displayName}</h3>
             <div className="Hobby">
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,10 +4,10 @@ export default function SearchBar({handleRandom, getProfile, profileItems}) {
     return (
         <div className="SearchBar" >
             <div className="SearchBar-items">
-                <button class={profileItems.length > 1 ? "red" : ""} onClick={getProfile}> Explore</button>
-                <button class={profileItems.length > 1 ? "" : "red"} onClick={handleRandom}> Match</button>
-                <label>
-                    Filter:
+                <button class={profileItems.length > 1 ? "red btn" : "btn"} onClick={getProfile}> Explore</button>
+                <button class={profileItems.length > 1 ? "btn" : "red btn"} onClick={handleRandom}> Match</button>
+                <label className="filter">
+                    <span>Sortby:</span>
                     <select>
                         <option value="All">All</option>
                         <option value="Age">Age</option>
