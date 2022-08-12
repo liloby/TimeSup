c49f418f5b9e536b4b768ad620841242903ca2eb
Oby <obyli214@gmail.com>
2022-08-12 10:33:16 -0700

    fix bug

--- src/components/RandomCard/RandomCard.jsx
+++ src/components/RandomCard/RandomCard.jsx
@@ -7,9 +7,9 @@ export default function PersonCard({ person }) {
         <div className="MatchCard">
             <img className="MatchCard-img" src={person.image} alt="" />
             <h3>{person.displayName}</h3>
-            <p>Age: {person.age}</p>
+            <p>{person.age}</p>
             <p>{person.hobbies}</p>
-            <p>Bio: {person.bio}</p>
+            <p>{person.bio}</p>
         </div>
     )
 }
