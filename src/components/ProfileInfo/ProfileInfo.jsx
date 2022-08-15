bfd848afde592e4e065bd351bc623ee6778ac411
Oby <obyli214@gmail.com>
2022-08-15 04:39:09 -0700

    editted Border Css

--- src/components/ProfileInfo/ProfileInfo.jsx
+++ src/components/ProfileInfo/ProfileInfo.jsx
@@ -1,17 +1,22 @@
-import "./ProfileInfo.css"
+import "./ProfileInfo.css";
 
-export default function ProfileInfo({currentProfile, updating, deleting}) {
-
-    return (
-        <div className="ProfileInfo">
-            <button onClick={updating} >Edit Profile</button>
-            <button onClick={deleting}>Delete Profile</button>
-            <h2>Current Displayed Profile</h2>
-            <img width="500px" src={currentProfile.image} alt="" />
-            <h3>{currentProfile.displayName}</h3>
-            <p>Bio: {currentProfile.bio}</p>
-            <p>Age: {currentProfile.age}</p>
-            <p>Hobbies: {currentProfile.hobbies}</p>
+export default function ProfileInfo({ currentProfile, updating, deleting }) {
+  return (
+    <div className="ProfileInfo">
+      <button onClick={updating}>Edit Profile</button>
+      <button onClick={deleting}>Delete Profile</button>
+      <h2>Current Displayed Profile</h2>
+      <div className="ProfileInfo-Wrapper">
+        <div>
+          <img className="Profile-image" width="500px" src={currentProfile.image} alt="" />
         </div>
-    )
+        <div className="ProfileStats">
+          <h2>{currentProfile.displayName}</h2>
+          <p>Bio: {currentProfile.bio}</p>
+          <p>Age: {currentProfile.age}</p>
+          <p>Hobbies: {currentProfile.hobbies}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
